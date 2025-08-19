e4d22bdabb8f550e96810d76e60a3a9505bf9cf5
Kleif298 <76910639+Kleif298@users.noreply.github.com>
2025-08-19 09:54:06 +0000

    Initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+    <header>
+        <img src="/src/assets/sunrise-logo-grey.png" alt="sunrise-logo-grey"></img>
+        <nav>
+            <a href="#about">About</a>
+            <a href="#projects">Projects</a>
+            <a href="#contact">Contact</a>
+        </nav>
+    </header>)
+};
+
+export default Header;
---
+++ src/components/hero.jsx
@@ -0,0 +1,10 @@
+const hero = () => {
+    return (
+        <div id="hero">
+            <h1 id="myname">Leif Fieger</h1>
+            <p>Explore my projects and skills</p>
+        </div>
+    )
+};
+
+export default hero;
