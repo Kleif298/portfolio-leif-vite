212f4a6cdc67a3884b2dff653e6c256be94332ec
Kleif298 <76910639+Kleif298@users.noreply.github.com>
2025-08-26 23:23:04 +0000

    logo import fixed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,9 +1,11 @@
+import sunriseLogo from "../assets/sunrise-logo-grey.png"
+
 const Header = () => {
     return (
     <header>
         <div className="edge-mini-bar"></div>
         <div className="edge-content">
-            <img src="/src/assets/sunrise-logo-grey.png" alt="sunrise-logo-grey"></img>
+            <img src={sunriseLogo} alt="sunrise-logo-grey"></img>
             <nav>
                 <a href="#">Home</a>
                 <a href="#about">About Me</a>
