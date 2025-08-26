e37ef548cf3ca9144786f901de7c83b5878155a8
Fieger <leif.fieger@sunrise.net>
2025-08-27 00:31:57 +0200

    Animationen und bessere Responsiveness von CoPilot

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Home from './pages/home'
+import ScrollToTop from './components/ScrollToTop'
 
 function App() {
-
   return (
     <>
       <Home />
+      <ScrollToTop />
     </>
   )
 }
